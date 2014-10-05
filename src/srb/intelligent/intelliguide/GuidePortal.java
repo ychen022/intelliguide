a364cb0334a12634d2d4fbf45411bbcca5598e67
Sam <mpan1218@mit.edu>
2014-10-05 01:54:49 -0400

    fuck you

--- src/srb/intelligent/intelliguide/GuidePortal.java
+++ src/srb/intelligent/intelliguide/GuidePortal.java
@@ -1,24 +1,23 @@
 package srb.intelligent.intelliguide;
 
-import java.lang.Math; 
+import java.text.DecimalFormat;
+import android.util.Log;
+import com.google.android.gms.maps.model.LatLng;
 
 public class GuidePortal {
-	public double[] cord = new double[2];
-	public double cordX;
-	public double cordY;
-	public String name;
-	public String guidePath;
-	public boolean played = false;
+	private LatLng cord;
+	private double cordX;
+	private double cordY;
+	private String name;
+	private String guidePath;
+	private boolean played = false; 
 	
-	public GuidePortal(double cordX, double cordY, String name, String path) {
-		this.cord[0] = cordX;
-		this.cord[1] = cordY;
+	public GuidePortal(LatLng cord, String name, String path) {
+		this.cord = cord;
+		this.cordX = cord.longitude;
+		this.cordY = cord.latitude;
 		this.name = name;
 		this.guidePath = path;
-	}
-	
-	public double[] getCordinate(double curX, double curY) {
-		return cord;
 	}
 	
 	public String loadGuide(){
@@ -26,8 +25,60 @@ public class GuidePortal {
 		return guidePath;
 	}
 	
-	public double getOrientation(double curX, double curY, double curDir){
+	public String getName(){
+		return name;
+	}
+	
+	public LatLng getCordinate(){
+		return cord;
+	}
+	
+	public double getDistance(LatLng cur){
+        int Radius=6371;
+        double lat1 = cordY;
+        double lon1 = cordX;
+        double lat2 = cur.latitude;
+        double lon2 = cur.longitude;
+        double dLat = Math.toRadians(lat2-lat1);
+        double dLon = Math.toRadians(lon2-lon1);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+        Math.sin(dLon/2) * Math.sin(dLon/2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        return Radius * c;
+	}
+	
+	public double getOrientation(LatLng cur, double curDir){
+		double curX = cur.longitude;
+		double curY = cur.latitude;
 		double theta = 180*Math.atan2((cordX-curX),(cordY-curY))/Math.PI;
-		return -1*theta+180;
+		double portalDir = -1*theta+180;
+		double angle = portalDir-curDir;
+		if(angle<0){angle = angle+360;}
+		return angle;
+	}
+	
+	public boolean inRange(LatLng cur, double curDir){
+		double d = getDistance(cur);
+		double theta = getOrientation(cur, curDir);
+		double reTheta;
+		if(theta>180){reTheta = (360-theta)/3;}
+		else{reTheta = theta/3;}
+		double bound = 0.5*Math.cos(reTheta);
+		if(bound>d){return true;}
+		else{return false;}
+	}
+	
+	public String inKM(LatLng cur, double curDir){
+		double d = getDistance(cur);
+		double theta = getOrientation(cur, curDir);
+		if(1>d){
+			if (theta>=45 && theta<135){return "rihgt";}
+			else if(theta>=135 && theta<225){return "back";}
+			else if(theta>=225 && theta<315){return "left";}
+			else{return "front";}
+		}else{
+			return null; 
+		}
 	}
 }
